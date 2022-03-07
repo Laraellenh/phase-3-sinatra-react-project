d0ca8920de9a51af683f1adbf62f9c03ef16d5c6
Lara <laraellenh@gmail.com>
2022-03-07 17:20:40 -0500

    working on the User method for sidebar. not done with it

--- app/models/Entry.rb
+++ app/models/Entry.rb
@@ -1,4 +1,10 @@
 class Entry < ActiveRecord::Base
     belongs_to :user
 
+    
+    # create a method to display the 5 most recent posts to be dislayed in the react components for entries
+    def self.last_five_entries
+        self.all.slice(-5, 5)
+    end
+
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
     has_many :entries
+
+    # write a method that displays the 3 users on the sidebar in React so we can sort through them
+    def display_users
+       team_hash = "#{user.first.name}
+       's most recent post "
+    end
+
 end
