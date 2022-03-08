b9db35cf79f988b9339eac4d1fb2673172493a78
Matt Quinn <mjq0533@gmail.com>
2022-03-08 09:34:44 -0600

    delte and post requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
+
+
+  # creates a new entry
+  post '/entries' do 
+    
+    new_entry = Entry.create(
+     date: params[:score]
+     emotion: params[:emotion]
+     note: params[:note]
+     user_id: params[:user_id]
+   )
+
+    new_entry.to_json
+  end
+
+
 
   #gets all entries
   get "/entries" do
@@ -10,6 +29,12 @@ class ApplicationController < Sinatra::Base
   end
 
   
+  #deletes an entry
+  delete "/entries/:id" do
+    delete_entry = Entry.find(params[:id])
+    delete_entry.destroy
+    delete_entry.to_json
+  end
 
 
   
